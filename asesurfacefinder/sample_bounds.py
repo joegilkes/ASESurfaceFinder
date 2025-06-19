438b57684c3080929a2e069f1217be9feb1028b7
Joe Gilkes <j.gilkes@warwick.ac.uk>
2025-06-19 11:20:05 +0100

    Added docstring for `SampleBounds`

--- asesurfacefinder/sample_bounds.py
+++ asesurfacefinder/sample_bounds.py
@@ -1,5 +1,16 @@
 class SampleBounds:
     def __init__(self, r_max: float, z_min: float, z_max: float, z_mid: float=None):
+        '''Container for surface site sampling bounds.
+
+        If `z_mid` is not provided, defines a hemispheroid with radius
+        `r_max`, base plane at `z_min` above the highest surface atom,
+        and apex at `z_max` above the highest surface atom.
+
+        If `z_mid` is provided, defines an ovoid with maximum radius
+        `r_max` at height `z_mid` above the highest surface atom.
+        Ovoid tapers to a lower apex at `z_min` and an upper apex at
+        `z_max`
+        '''
         if r_max < 0:
             raise ValueError('Sampling r_max must be greater than or equal to zero.')
         if z_mid is not None:
